ff49093957ed7f41176b5918faa465722ecf80b5
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2019-05-19 16:39:53 +0200

    chrome ext

---
+++ chrome_extension/background.js
@@ -0,0 +1,26 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log('The color is green.');
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'developer.chrome.com'},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'www.youtube.com'},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+
+});
---
+++ chrome_extension/popup.js
@@ -0,0 +1,77 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason == "install") {
+    chrome.app.window.create('options.html', {
+      bounds: {
+        'width': 1024,
+        'height': 768
+       }
+      
+       });
+  } 
+});
+
+
+
+function load_popup(data){
+
+  if('email' in data){
+
+    document.getElementById("configured").style.display="inline";
+    document.getElementById("not-configured").style.display="none";
+
+    
+    document.getElementById("email").innerHTML=data.email;
+    
+    document.querySelector("#logslink a").onclick = function () {
+      chrome.tabs.create({active: true, url: "https://streaming-sniffer-api.nextnet.top/"+data.email+"/logs"});
+  };
+    
+  
+    changeColor.onclick = function(element) {
+      
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code:`
+            for(let a of document.querySelectorAll("#content #contents #contents a#thumbnail")){
+              console.log(a.href.substring(32,43));
+              const xhr = new XMLHttpRequest();
+              xhr.open('POST', 'https://streaming-sniffer-api.nextnet.top/`+data.email+`/'+a.href.substring(32,43));
+              xhr.send();
+            }
+            `});
+      });
+    };
+    }
+    else{
+  
+      document.getElementById("not-configured").style.display="inline";
+      document.getElementById("configured").style.display="none";
+      let submit = document.getElementById('submit');
+  submit.addEventListener('click', function() {
+        let email=document.getElementById("name").value;
+        chrome.storage.sync.set({email: email}, function() {
+          console.log('email saved ' + email);
+          const xhr = new XMLHttpRequest();
+          xhr.open('POST', 'https://streaming-sniffer-api.nextnet.top/'+email);
+          xhr.send();
+          xhr.onreadystatechange=function(){
+            if (xhr.readyState==4 && xhr.status==200){
+              load_popup(data);
+            }
+          };
+        });
+      });
+    }
+  
+}
+chrome.storage.sync.get(['email'], function(data) {
+  load_popup(data);
+});
+
